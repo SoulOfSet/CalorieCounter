a369aa6e8d28dee87da3d696f85536c7d79a2728
wagstaffj <wagstaffj@umsl.edu>
2021-12-03 22:00:52 -0600

    New form for food entry and report
    Created new form for food entry and report and added components.

---
+++ CalorieCounter/FoodEntry.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace CalorieCounter
+{
+    partial class FoodEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.Type = new System.Windows.Forms.Label();
+            this.units = new System.Windows.Forms.Label();
+            this.calories = new System.Windows.Forms.Label();
+            this.carbs = new System.Windows.Forms.Label();
+            this.protein = new System.Windows.Forms.Label();
+            this.fat = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.save = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(95, 78);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(35, 13);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Name";
+            // 
+            // Type
+            // 
+            this.Type.AutoSize = true;
+            this.Type.Location = new System.Drawing.Point(95, 108);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(31, 13);
+            this.Type.TabIndex = 1;
+            this.Type.Text = "Type";
+            // 
+            // units
+            // 
+            this.units.AutoSize = true;
+            this.units.Location = new System.Drawing.Point(95, 141);
+            this.units.Name = "units";
+            this.units.Size = new System.Drawing.Size(31, 13);
+            this.units.TabIndex = 2;
+            this.units.Text = "Units";
+            // 
+            // calories
+            // 
+            this.calories.AutoSize = true;
+            this.calories.Location = new System.Drawing.Point(95, 166);
+            this.calories.Name = "calories";
+            this.calories.Size = new System.Drawing.Size(44, 13);
+            this.calories.TabIndex = 3;
+            this.calories.Text = "Calories";
+            // 
+            // carbs
+            // 
+            this.carbs.AutoSize = true;
+            this.carbs.Location = new System.Drawing.Point(95, 194);
+            this.carbs.Name = "carbs";
+            this.carbs.Size = new System.Drawing.Size(34, 13);
+            this.carbs.TabIndex = 4;
+            this.carbs.Text = "Carbs";
+            // 
+            // protein
+            // 
+            this.protein.AutoSize = true;
+            this.protein.Location = new System.Drawing.Point(95, 222);
+            this.protein.Name = "protein";
+            this.protein.Size = new System.Drawing.Size(40, 13);
+            this.protein.TabIndex = 5;
+            this.protein.Text = "Protein";
+            // 
+            // fat
+            // 
+            this.fat.AutoSize = true;
+            this.fat.Location = new System.Drawing.Point(95, 251);
+            this.fat.Name = "fat";
+            this.fat.Size = new System.Drawing.Size(22, 13);
+            this.fat.TabIndex = 6;
+            this.fat.Text = "Fat";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(160, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(160, 186);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(160, 214);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(160, 251);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(160, 46);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(160, 305);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 14;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(241, 305);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 15;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(160, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // FoodEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.fat);
+            this.Controls.Add(this.protein);
+            this.Controls.Add(this.carbs);
+            this.Controls.Add(this.calories);
+            this.Controls.Add(this.units);
+            this.Controls.Add(this.Type);
+            this.Controls.Add(this.Name);
+            this.Name = "FoodEntry";
+            this.Load += new System.EventHandler(this.FoodEntry_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label Type;
+        private System.Windows.Forms.Label units;
+        private System.Windows.Forms.Label calories;
+        private System.Windows.Forms.Label carbs;
+        private System.Windows.Forms.Label protein;
+        private System.Windows.Forms.Label fat;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
